f938d2a9c5e9f9aa6ec336a3d4d71a5431535877
SamuelDac <samuel@dacxi.com>
2022-08-31 19:45:58 -0300

    Making a simple server to test a docker container

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ exports.__esModule = true;
 var sum_1 = require("./sum/sum");
 var result = (0, sum_1.sum)(2, 2);
 console.log(result);
+var express = require("express");
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, function () {
+    console.log("[server]: Server is running at https://localhost:".concat(port));
+});
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,16 @@ import { sum } from "./sum/sum"
 
 const result = sum(2, 2)
 console.log(result)
+
+const express = require("express")
+import { Express, Request, Response } from 'express'
+const app: Express = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
